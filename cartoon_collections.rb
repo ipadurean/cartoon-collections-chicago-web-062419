d25f40b03fc536c520556546e05c9b1e0db1e0f0
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-21 18:49:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(names)# code an argument here
+  names.map do |el| el.capitalize  + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
